dd26d8d1bf68b545ce67ea790c7272de840740cd
ladauxiliary <ladwow@list.ru>
2022-12-06 19:33:52 +0200

    USART base

--- main.c
+++ main.c
@@ -8,19 +8,36 @@ void delay(uint32_t ticks) {
 }
 
 int __attribute((noreturn)) main(void) {
-	// Enable clock for AFIO
 	RCC->APB2ENR |= RCC_APB2ENR_AFIOEN;
-	// Enable clock for GPIOC
-	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	RCC->APB1ENR |= RCC_APB1ENR_USART3EN;
 	// Enable PC13 push-pull mode
-	GPIOC->CRH &= ~GPIO_CRH_CNF13; //clear cnf bits
-	GPIOC->CRH |= GPIO_CRH_MODE13_0; //Max speed = 10Mhz
+	GPIOB->CRH &= ~GPIO_CRH_CNF10; 
+	GPIOB->CRH |= GPIO_CRH_CNF10_1; 
 
-    while (1) {
-	    GPIOC->ODR |= (1U<<13U); //U -- unsigned suffix (to avoid syntax warnings in IDE)
-		delay(1000000);
-	    GPIOC->ODR &= ~(1U<<13U);
-	    delay(1000000);
-		//
-    }
+	GPIOB -> CRH &= ~GPIO_CRH_MODE10;
+	GPIOB -> CRH |= ~GPIO_CRH_MODE10_0;
+
+	GPIOB->CRH &= ~GPIO_CRH_CNF11; 
+	GPIOB->CRH |= GPIO_CRH_CNF11_1; 
+	GPIOB -> CRH |= ~GPIO_CRH_MODE11;
+
+	GPIOB->ODR |=GPIO_ODR_ODR11;
+
+	USART3->BRR = (1875<<4);
+	USART3->CR1 |= USART_CR1_UE;
+	USART3->CR1 |= USART_CR1_TE;
+	USART3->CR1 |= USART_CR1_RE;
+
+	RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;
+	GPIOC->CRH &= ~GPIO_CRH_CNF13;
+	GPIOC->CRH |= GPIO_CRH_MODE13_0;
+
+	while (1)
+	{
+		GPIOC->ODR |= (1U<<13U);
+		delay(100000);
+		GPIOC->ODR &= ~(1U<<13U);
+		USART3 ->DR = 'D';
+		delay(100000);
+	}
 }
